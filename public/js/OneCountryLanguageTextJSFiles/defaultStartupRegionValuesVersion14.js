9b30b433cb8f4e7c9575358bc43b17553fc4aa37
Mehmet Erten <mferten@yahoo.com>
2019-02-12 18:49:47 -0500

    commit all

--- public/js/OneCountryLanguageTextJSFiles/defaultStartupRegionValuesVersion14.js
+++ public/js/OneCountryLanguageTextJSFiles/defaultStartupRegionValuesVersion14.js
@@ -5,8 +5,8 @@ var DEFAULTREGIONAPPLICATIONCOUNTRYANDLANGUAGE = "Slovakia-Slovak";
 var DEFAULTREGION = "Europe";
 
 //  Keep Adding new Application Languages (both Text and Data)
-var selectedApplicationLanguageName = {"English (US)":["Anglický", "UnitedStatesofAmerica", "http://www.k12k20.com"],
-    "Turkish":["Turecký", "Turkey", "http://www.k12k20tr.com"], "Slovak":["Slovenský", "Slovakia", "http://www.k12k20sk.com"]};
+var selectedApplicationLanguageName = {"Slovak":["Slovenský", "Slovakia", "http://www.k12k20sk.com"],
+    "English (US)":["Anglický", "UnitedStatesofAmerica", "http://www.k12k20.com"], "Turkish":["Turecký", "Turkey", "http://www.k12k20tr.com"]};
 
 // DEFAULT REGION Application Start UP Values which will be used to load into Local Storage Area
 // console.log(encodeURIComponent(JSON.stringify(selectedApplicationLanguageTexts))) and encodeURIComponent(JSON.stringify(languageOfCountries)
