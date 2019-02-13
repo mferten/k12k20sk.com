d2b69a187c7b28b545c7cdf81ed9bd424bb9ffac
Mehmet Erten <mferten@yahoo.com>
2019-02-12 19:04:14 -0500

    commit all

--- public/js/utilityForAllVersion14.js
+++ public/js/utilityForAllVersion14.js
@@ -723,8 +723,8 @@ function addApplicationLanguageSelectionDropDownBox(addIntoTag) // See Global on
     dropDownTopDiv.setAttribute("class", "dropdown");
     var dropDownButton = document.createElement("button");
     dropDownButton.setAttribute("class", "dropbtn");
-    createAnImageInA(dropDownButton, selectedApplicationLanguageName["English (US)"][0],
-        selectedApplicationLanguageName["English (US)"][1], selectedApplicationLanguageName["English (US)"][2], "dropDownFlagText", true);
+    createAnImageInA(dropDownButton, selectedApplicationLanguageName["Slovak"][0],
+        selectedApplicationLanguageName["Slovak"][1], selectedApplicationLanguageName["Slovak"][2], "dropDownFlagText", true);
     dropDownTopDiv.appendChild(dropDownButton);
     var dropDownInnerDiv = document.createElement("div");
     dropDownInnerDiv.setAttribute("class", "dropdown-content");
